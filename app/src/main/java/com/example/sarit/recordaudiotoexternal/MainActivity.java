765c4a8164cec71e328b84c1c0e5d06d2efcac7c
Sarit <Sarit@shannon.tch.harvard.edu>
2018-09-06 14:06:22 -0400

    Added four response buttons

--- app/src/main/java/com/example/sarit/recordaudiotoexternal/MainActivity.java
+++ app/src/main/java/com/example/sarit/recordaudiotoexternal/MainActivity.java
@@ -14,66 +14,37 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.ToggleButton;
+
 import java.io.IOException;
+import java.util.Date;
+
 import static android.Manifest.permission.RECORD_AUDIO;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public class MainActivity extends AppCompatActivity {
-    private Button startbtn, stopbtn, playbtn, stopplay;
+    private ToggleButton startRecording, playRecording;
     private MediaRecorder mRecorder;
     private MediaPlayer mPlayer;
     private static final String LOG_TAG = "AudioRecording";
     private static String mFileName = null;
     public static final int REQUEST_AUDIO_PERMISSION_CODE = 1;
     boolean isRecording = false;
-
+    boolean isPlaying = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(com.example.sarit.recordaudiotoexternal.R.layout.activity_main);
-        startbtn = findViewById(com.example.sarit.recordaudiotoexternal.R.id.btnRecord);
-        stopbtn = findViewById(com.example.sarit.recordaudiotoexternal.R.id.btnStop);
-        playbtn = findViewById(R.id.btnPlay);
-        stopplay = findViewById(R.id.btnStopPlay);
-        stopbtn.setEnabled(false);
-        playbtn.setEnabled(false);
-        stopplay.setEnabled(false);
+        startRecording = findViewById(com.example.sarit.recordaudiotoexternal.R.id.startRecording);
+        playRecording = findViewById(com.example.sarit.recordaudiotoexternal.R.id.playRecording);
+        playRecording.setEnabled(false);
+
         //mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
         mFileName = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PODCASTS).getAbsolutePath();
 
         Toast.makeText(MainActivity.this,mFileName.toString(),Toast.LENGTH_SHORT).show();
         mFileName += "/AudioRecording.3gp";
 
-        playbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                stopbtn.setEnabled(false);
-                startbtn.setEnabled(true);
-                playbtn.setEnabled(false);
-                stopplay.setEnabled(true);
-                mPlayer = new MediaPlayer();
-                try {
-                    mPlayer.setDataSource(mFileName);
-                    mPlayer.prepare();
-                    mPlayer.start();
-                    Toast.makeText(getApplicationContext(), "Recording Started Playing", Toast.LENGTH_LONG).show();
-                } catch (IOException e) {
-                    Log.e(LOG_TAG, "prepare() failed");
-                }
-            }
-        });
-        stopplay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mPlayer.release();
-                mPlayer = null;
-                stopbtn.setEnabled(false);
-                startbtn.setEnabled(true);
-                playbtn.setEnabled(true);
-                stopplay.setEnabled(false);
-                Toast.makeText(getApplicationContext(),"Playing Audio Stopped", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
@@ -100,15 +71,29 @@ public class MainActivity extends AppCompatActivity {
         ActivityCompat.requestPermissions(MainActivity.this, new String[]{RECORD_AUDIO, WRITE_EXTERNAL_STORAGE}, REQUEST_AUDIO_PERMISSION_CODE);
     }
 
+    public void startPlaying(View view) {
+        if (!isPlaying){
+            startRecording.setEnabled(true);
+            mPlayer = new MediaPlayer();
+            try {
+                mPlayer.setDataSource(mFileName);
+                mPlayer.prepare();
+                mPlayer.start();
+                Toast.makeText(getApplicationContext(), "Recording Started Playing", Toast.LENGTH_LONG).show();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "prepare() failed");
+            }
 
+        }
+        else if (isPlaying){
+            mPlayer.release();
+            mPlayer = null;
+            Toast.makeText(getApplicationContext(),"Playing Audio Stopped", Toast.LENGTH_SHORT).show();
+        }
+    }
     public void startRecording(View view){
         if(CheckPermissions()) {
             if (!isRecording) {
-
-                stopbtn.setEnabled(true);
-                startbtn.setEnabled(false);
-                playbtn.setEnabled(false);
-                stopplay.setEnabled(false);
                 mRecorder = new MediaRecorder();
                 mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
                 mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
@@ -123,13 +108,11 @@ public class MainActivity extends AppCompatActivity {
                 mRecorder.start();
                 isRecording = true;
                 Toast.makeText(getApplicationContext(), "Recording Started", Toast.LENGTH_LONG).show();
+                playRecording.setEnabled(true);
+
             } else if (isRecording) {
                 Toast.makeText(MainActivity.this, "Stopped recording", Toast.LENGTH_SHORT).show();
-                stopbtn.setEnabled(false);
-                startbtn.setEnabled(true);
-                playbtn.setEnabled(true);
-                stopplay.setEnabled(true);
-                mRecorder.stop();
+                 mRecorder.stop();
                 mRecorder.release();
                 mRecorder = null;
                 Toast.makeText(getApplicationContext(), "Recording Stopped", Toast.LENGTH_LONG).show();
@@ -138,6 +121,40 @@ public class MainActivity extends AppCompatActivity {
         else {
                 RequestPermissions();
         }
+    }
+
+    public void findButton(View v){
+        switch (v.getId()) {
+
+            case R.id.good_y:
+                Toast.makeText(MainActivity.this, "good_y clicked", Toast.LENGTH_SHORT).show();
+
+                break;
+
+            case R.id.great_b:
+                Toast.makeText(MainActivity.this, "great_b clicked", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.knewit_x:
+                Toast.makeText(MainActivity.this, "knewit_x clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.notuseful_a:
+                Toast.makeText(MainActivity.this, "notuseful_a clicked", Toast.LENGTH_SHORT).show();
+
+                break;
+            default:
+                break;
+        }
+
+
+    }
+
+    public String updateTime() {
+        //java.util.Date() method is used to get the current time
+        String time = new Date().toString();
+        Toast.makeText(getApplicationContext(),
+                time, Toast.LENGTH_SHORT).show();
+        return time;
     }
 }
 
